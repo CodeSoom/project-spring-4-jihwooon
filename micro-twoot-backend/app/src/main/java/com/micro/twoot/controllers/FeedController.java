b944faceff3ec688feae21e9c4a624f69c6569ab
jihwooon <jihwooon@gmail.com>
2022-01-24 23:23:58 +0900

    Add : FeedController 기능 추가
    Mapping 기능 추가
    
    Get, Post, Put, Delete

--- micro-twoot-backend/app/src/main/java/com/micro/twoot/controllers/FeedController.java
+++ micro-twoot-backend/app/src/main/java/com/micro/twoot/controllers/FeedController.java
@@ -6,9 +6,35 @@
 // 5. Delete
 package com.micro.twoot.controllers;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/feed")
 public class FeedController {
+
+    @GetMapping
+    public String list() {
+        return "List";
+    }
+
+    @PostMapping
+    public String create() {
+        return "Post";
+    }
+
+    @PutMapping
+    public String update() {
+        return "Update";
+    }
+
+    @DeleteMapping
+    public String delete() {
+        return "Delete";
+    }
 
 }
