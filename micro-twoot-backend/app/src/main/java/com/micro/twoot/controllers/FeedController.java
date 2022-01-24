31744fd2b2cc5ad668dffec9adee6a69c3bca29d
jihwooon <jihwooon@gmail.com>
2022-01-24 22:43:13 +0900

    Add : FeedController 추가
    FeedController 추가
    
    Read, Create, Update, Delete 기능 추가 예정

---
+++ micro-twoot-backend/app/src/main/java/com/micro/twoot/controllers/FeedController.java
@@ -0,0 +1,14 @@
+//TODO
+// 1. Read Collection = GET /feed
+// 2. Read
+// 3. Create
+// 4. Update
+// 5. Delete
+package com.micro.twoot.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FeedController {
+
+}
