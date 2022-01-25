eecd7a5ba9940197552791754bde23f844ffbe22
jihwooon <jihwooon@gmail.com>
2022-01-25 12:43:02 +0900

    Test : feed 객체 테스트
    간단한 id, content 값 테스트 완료

---
+++ micro-twoot-backend/app/src/test/java/com/micro/twoot/models/FeedTest.java
@@ -0,0 +1,22 @@
+package com.micro.twoot.models;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+class FeedTest {
+
+    @Test
+    void createFeed() {
+        Feed feed = new Feed();
+        feed.setId(1L);
+        feed.setContent("트윗!");
+
+        assertThat(feed.getId()).isEqualTo(1L);
+        assertThat(feed.getId()).isNotEqualTo(2L);
+        assertThat(feed.getContent()).isNotEqualTo("트우트!");
+        assertThat(feed.getContent()).isEqualTo("트윗!");
+
+    }
+}
+
