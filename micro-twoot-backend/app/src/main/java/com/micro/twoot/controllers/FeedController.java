bfa0ed9e79930e57060df632a2a5e07e52592cd1
jihwooon <jihwooon@gmail.com>
2022-01-25 18:22:58 +0900

    Fix : 'FeedController'
    Fix the `FeedController` class to represent controller domain
    
    It contains only 4 method just now : `list`, `read` , `create` and `delete`.

--- micro-twoot-backend/app/src/main/java/com/micro/twoot/controllers/FeedController.java
+++ micro-twoot-backend/app/src/main/java/com/micro/twoot/controllers/FeedController.java
@@ -1,9 +1,3 @@
-//TODO
-// 1. Read Collection = GET /feed
-// 2. Read
-// 3. Create
-// 4. Update
-// 5. Delete
 package com.micro.twoot.controllers;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
---
+++ micro-twoot-backend/app/src/test/java/com/micro/twoot/models/FeedTest.java
@@ -0,0 +1,20 @@
+package com.micro.twoot.models;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+class FeedTest {
+
+    @Test
+    void createFeed() {
+        Feed feed = new Feed(1L, "트윗!");
+
+        assertThat(feed.getId()).isEqualTo(1L);
+        assertThat(feed.getId()).isNotEqualTo(2L);
+        assertThat(feed.getContent()).isNotEqualTo("트우트!");
+        assertThat(feed.getContent()).isEqualTo("트윗!");
+
+    }
+}
+
