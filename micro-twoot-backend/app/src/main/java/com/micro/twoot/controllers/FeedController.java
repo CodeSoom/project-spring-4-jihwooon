8e0dd22b37ce31bc68d7716eec4f92e34ecb64ab
jihwooon <jihwooon@gmail.com>
2022-01-26 10:42:27 +0900

    Create `FeedController`
    Make `List`,`Detail`,`Create`,`Update`,`Delete`
    And If Test Passed, continues process

--- micro-twoot-backend/app/src/main/java/com/micro/twoot/controllers/FeedController.java
+++ micro-twoot-backend/app/src/main/java/com/micro/twoot/controllers/FeedController.java
@@ -1,34 +1,53 @@
 package com.micro.twoot.controllers;
 
+import com.micro.twoot.models.Feed;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/feed")
+@RequestMapping("/feeds")
 public class FeedController {
+    private List<Feed> feeds = new ArrayList<>();
+    private Long newId = 0L;
 
     @GetMapping
-    public String list() {
-        return "List";
+    public List<Feed> list() {
+        return feeds;
     }
 
     @PostMapping
-    public String create() {
-        return "Post";
+    public Feed create(@RequestBody Feed feed) {
+        feed.setId(generateId());
+        feeds.add(feed);
+
+        return null;
     }
 
-    @PutMapping
-    public String update() {
-        return "Update";
+    @PutMapping({"id"})
+    public Feed update() {
+        return null;
+
     }
 
-    @DeleteMapping
-    public String delete() {
-        return "Delete";
+    @DeleteMapping("id")
+    public Feed delete() {
+
+        return null;
+
+    }
+
+
+
+    private Long generateId() {
+        newId += 1;
+        return newId;
     }
 
 }
